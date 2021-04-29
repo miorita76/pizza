4243e5cda59a9806d6b67b7834615eac575520c4
miorita76 <71668122+miorita76@users.noreply.github.com>
2021-04-29 13:02:36 +0300

    wrote the functional of the script

--- js/recipe-erase.js
+++ js/recipe-erase.js
@@ -1,7 +1,18 @@
-
-// Cancel View Scaffold board
+// Erase Recipe Scaffold board
 function eraseScaffoldRecipe() {
-    console.log("INTERIOR  ERASE");
+
+    let recipesLocal = localStorage.getItem('recipes');
+    recipesLocal = JSON.parse(recipesLocal);
+
+    selectedRecipeIndex = selectedRecipeIndex - DEFAULT_RECIPE_LENGTH;
+    recipesLocal.splice(selectedRecipeIndex, 1);
+
+    localStorage.setItem('recipes', JSON.stringify(recipesLocal));
+    recipesList = recipesDefault.concat(recipesLocal);
+    clearBoard('#recipe-board_list');
+    let fullRecipesErase = new RecipesBoard(recipesList, DEFAULT_RECIPE_LENGTH);
+    fullRecipesErase.boardDraw();
+
     document.getElementById("recipe-scaffolding").style.display = "none";
     document.getElementById("recipe-display").classList.add("visually-hidden");
     document.getElementById("main-board").classList.remove("visually-hidden");
