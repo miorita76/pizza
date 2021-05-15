5ba2faaba6c6c63b27db30e5ded0c4d399dbfa40
miorita76 <71668122+miorita76@users.noreply.github.com>
2021-05-15 19:36:03 +0300

    recipe highlighting

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// console.log("verificare conexiune SCRIPT");
 
 // Clear board to be populated 
 function clearBoard(boardName) {
@@ -17,4 +16,75 @@ function clearSelected() {
     recipesClear.boardDraw();
 
     cancelScaffoldModify();
+}
+
+
+let highlightRecipeList = [];
+
+function displayIngredientValue() {
+    let ingredientsSelected = document.getElementsByName('ingredient_item');
+    let elementSelectedIngredient = [];
+
+    // identify selected Ingredients array
+    for (i = 0; i < ingredientsSelected.length; i++) {
+        if (ingredientsSelected[i].checked) {
+            elementSelectedIngredient.push(ingredientsSelected[i].value);
+        }
+    };
+
+    if (elementSelectedIngredient.length == 0 || elementSelectedIngredient == undefined) {
+        console.log('No ingerdient elements selected !');
+        // Clear and draw recipe board
+        clearBoard('#recipe-board_list');
+        let recipesNoIngredients = new RecipesBoard(recipesList);
+        recipesNoIngredients.boardDraw();
+    } else {
+        // formation of higlight recipes
+        console.log(elementSelectedIngredient);
+
+        let flag = [];
+        let highlightRecipeList = [];
+
+        // verification of multiple elements
+        function logicResult(myArray) {
+            let sum = true;
+            for (let ii = 0; ii < myArray.length; ii++) {
+                sum = sum && myArray[ii];
+            }
+            return sum;
+        };
+
+
+        if (elementSelectedIngredient.length != 0) {
+            for (let i = 0; i < recipesList.length; i++) {
+                // verification by forming of the boolean array
+                for (let k = 0; k < elementSelectedIngredient.length; k++) {
+                    flag[k] = recipesList[i].ingredients.includes(elementSelectedIngredient[k]);
+                };
+
+                // formation of the highlightRecipeList array
+                if (logicResult(flag) == true) {
+                    // console.log(recipesList[i].name);
+                    highlightRecipeList.push(recipesList[i].name);
+                };
+            };
+
+            // Clear and draw recipe board
+            clearBoard('#recipe-board_list');
+            let recipesHighlight = new RecipesBoard(recipesList, highlightRecipeList);
+            recipesHighlight.boardDraw();
+        }
+        else {
+            // nothing selected
+            console.log('elementSelectedIngredient.length= ' + elementSelectedIngredient.length);
+
+            // Clear and draw recipe board
+            clearBoard('#recipe-board_list');
+            let recipesClear = new RecipesBoard(recipesList, null);
+            recipesClear.boardDraw();
+        };
+
+
+    };
+
 }
