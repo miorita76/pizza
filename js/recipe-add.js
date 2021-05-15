055ad85713e91e5c8296566955f893bbc6c9b180
miorita76 <71668122+miorita76@users.noreply.github.com>
2021-05-15 14:10:18 +0300

    rewrote Recipes blocks

--- js/recipe-add.js
+++ js/recipe-add.js
@@ -1,7 +1,6 @@
 
 function saveScaffoldAdd() {
 
-
     let pizza_name = document.getElementById("recipe-add_name").value;
 
     if (!pizza_name) {
@@ -29,9 +28,9 @@ function saveScaffoldAdd() {
         document.getElementById("recipe-scaffolding").style.display = "block";
     };
 
-    let ingredientsSelected = document.getElementsByName('ingredient_item');
-    let elementSelected = [];
 
+    let ingredientsSelected = document.getElementsByName('recipe-ingredient_item_add');
+    let elementSelected = [];
     // identify selected Ingredients array
     for (i = 0; i < ingredientsSelected.length; i++) {
         if (ingredientsSelected[i].checked) {
@@ -51,7 +50,7 @@ function saveScaffoldAdd() {
     };
 
     // verification if ingredient details is empty
-    let ingredients_details = document.getElementById("recipe-add_ingredients-details").value;
+    let ingredients_details = document.getElementById("recipe-add_details").value;
     if (ingredients_details == '') {
         document.getElementById("recipe-error_detailAdd").classList.remove("visually-hidden");
         //console.log('No Ingredients Details Inserted !!')
@@ -73,7 +72,8 @@ function saveScaffoldAdd() {
     };
 
     // data existance verification
-    if ((flag == false) && (elementSelected.length !== 0) && (elementSelected !== undefined) && (ingredients_details !== '') && (recipe_description !== '')) {
+    if ((flag == false) && (elementSelected.length !== 0) && (elementSelected !== undefined)
+        && (ingredients_details !== '') && (recipe_description !== '')) {
 
         // create temporary object to be PUSH in recipesList
         let temp = new Object();
@@ -82,13 +82,13 @@ function saveScaffoldAdd() {
         temp.ingredients = elementSelected;
         temp.details = ingredients_details;
         temp.description = recipe_description;
+        //console.log(temp);
 
         let recipesLocal = localStorage.getItem('recipes');
         recipesLocal = JSON.parse(recipesLocal);
 
         if (typeof recipesLocal != "undefined" && recipesLocal != null && recipesLocal.length != null && recipesLocal.length > 0) {
             // console.log('recipesLocal EXISTA');
-
             recipesLocal.push(temp);
             // sorting by name
             recipesLocal.sort(function (a, b) {
@@ -102,9 +102,9 @@ function saveScaffoldAdd() {
             });
 
             localStorage.setItem('recipes', JSON.stringify(recipesLocal));
-            recipesList = recipesDefault.concat(recipesLocal);
+            recipesList = recipesLocal;
             clearBoard('#recipe-board_list');
-            let fullRecipesNext = new RecipesBoard(recipesList, DEFAULT_RECIPE_LENGTH, null);
+            let fullRecipesNext = new RecipesBoard(recipesList);
             fullRecipesNext.boardDraw();
 
             document.getElementById("recipe-scaffolding").style.display = "none";
@@ -112,17 +112,17 @@ function saveScaffoldAdd() {
             document.getElementById("main-board").classList.remove("visually-hidden");
         }
         else {
-            // console.log('recipesLocal LIPSESTE');
-            recipesLocal = [];
+            //console.log('recipesLocal LIPSESTE');
+            recipesLocal = recipesDefault;
             recipesLocal.push(temp);
 
             localStorage.setItem('recipes', JSON.stringify(recipesLocal));
 
-            recipesList = recipesDefault.concat(recipesLocal);
+            recipesList = recipesLocal;
 
             // Clear and draw recipe board
             clearBoard('#recipe-board_list');
-            let fullRecipesInitial = new RecipesBoard(recipesList, DEFAULT_RECIPE_LENGTH, null);
+            let fullRecipesInitial = new RecipesBoard(recipesList);
             fullRecipesInitial.boardDraw();
 
             document.getElementById("recipe-scaffolding").style.display = "none";
@@ -130,47 +130,70 @@ function saveScaffoldAdd() {
             document.getElementById("main-board").classList.remove("visually-hidden");
         };
 
+        // clear and draw ingredients
+        document.getElementById("ingredient-form").reset();
+        let recipeIngredientsLocal = localStorage.getItem('ingredients');
+        recipeIngredientsLocal = JSON.parse(recipeIngredientsLocal);
     };
-
 };
 document.querySelector('#btn-add_recipe').onclick = saveScaffoldAdd;
 
+
+// Open ADD Scaffold board from recipice board
+function viewScaffoldAdd() {
+
+    // Clear and draw recipe board
+    clearBoard('#recipe-board_list');
+    let fullRecipesInitial = new RecipesBoard(recipesList);
+    fullRecipesInitial.boardDraw();
+
+    // clear Name field in case of previous insertion
+    document.getElementById("recipe-add_name").value = '';
+
+    document.getElementById("recipe-modify").classList.add("visually-hidden");
+    document.getElementById("recipe-display").classList.add("visually-hidden");
+    document.getElementById("recipe-error_nameAdd").classList.add("visually-hidden");
+    document.getElementById("recipe-error_nameAdd_exist").classList.add("visually-hidden");
+    document.getElementById("recipe-error_ingredientAdd").classList.add("visually-hidden");
+    document.getElementById("recipe-error_detailAdd").classList.add("visually-hidden");
+    document.getElementById("recipe-error_descriptionAdd").classList.add("visually-hidden");
+    document.getElementById("recipe-scaffolding").style.display = "block";
+    document.getElementById("recipe-add").classList.remove("visually-hidden");
+    document.getElementById("main-board").classList.add("visually-hidden");
+
+    // clear and draw ingredients
+    document.getElementById("ingredient-form").reset();
+    let recipeIngredientsLocal = localStorage.getItem('ingredients');
+    recipeIngredientsLocal = JSON.parse(recipeIngredientsLocal);
+
+    clearBoard("#recipe-scaffolding_left_add");
+    clearBoard("#recipe-scaffolding_right_add");
+
+    let ingredientsDrawList = new IngredientsScaffolding(recipeIngredientsLocal, '_add');
+    ingredientsDrawList.boardDraw();
+
+    // clear Selected fields in case previous inserted smth
+    let ingredientsSelected = document.getElementsByName('recipe-ingredient_item_add');
+    for (let i = 0; i < ingredientsSelected.length; i++) {
+        ingredientsSelected[i].checked = false;
+    };
+
+    // clear Details and Description fields in case of previous insertion
+    document.getElementById("recipe-add_details").value = "";
+    document.getElementById("recipe-add_description").value = "";
+};
+document.querySelector('#recipe-board_add').onclick = viewScaffoldAdd;
+
+
 // Cancel ADD Scaffold board
 function cancelScaffoldAdd() {
+    // clear and draw ingredients
     document.getElementById("ingredient-form").reset();
+    let recipeIngredientsLocal = localStorage.getItem('ingredients');
+    recipeIngredientsLocal = JSON.parse(recipeIngredientsLocal);
 
     document.getElementById("recipe-scaffolding").style.display = "none";
     document.getElementById("recipe-add").classList.add("visually-hidden");
     document.getElementById("main-board").classList.remove("visually-hidden");
 };
-document.querySelector('#btn-cancel_recipe').onclick = cancelScaffoldAdd;
-
-// Open ADD Scaffold board from recipice board
-function viewScaffoldAdd() {
-    document.getElementById("recipe-modify").classList.add("visually-hidden");
-    document.getElementById("recipe-display").classList.add("visually-hidden");
-    document.getElementById("recipe-error_nameAdd").classList.add("visually-hidden");
-    document.getElementById("recipe-error_ingredientAdd").classList.add("visually-hidden");
-    document.getElementById("recipe-error_detailAdd").classList.add("visually-hidden");
-    document.getElementById("recipe-scaffolding").style.display = "block";
-    document.getElementById("recipe-add").classList.remove("visually-hidden");
-    document.getElementById("main-board").classList.add("visually-hidden");
-
-    document.getElementById("ingredient-form").reset();
-    let ingredientsLocal = localStorage.getItem('ingredients');
-    ingredientsLocal = JSON.parse(ingredientsLocal);
-
-    if (typeof ingredientsLocal != "undefined" && ingredientsLocal != null) {
-        ingredientsList = ingredientsDefault.concat(ingredientsLocal);
-    }
-    else {
-        ingredientsList = ingredientsDefault;
-    };
-
-    clearBoard('#recipe-scaffolding_left');
-    clearBoard('#recipe-scaffolding_right');
-    let ingredientsRecipe = new IngredientsBoard(ingredientsList, DEFAULT_INGREDIENT_LENGTH);
-    ingredientsRecipe.ingredientsScaffolding();
-
-};
-document.querySelector('#btn-recipesAdd').onclick = viewScaffoldAdd;
+document.querySelector('#btn-add-cancel_recipe').onclick = cancelScaffoldAdd;
